d4f833282ee751cfd7692aab843fb206dd0c2e12
3ay <lefrost.volkoff@yandex.ru>
2023-04-26 17:49:09 +0300

    refactor directories

--- src/test/java/ru/netology/geo/GeoServiceImplTest.java
+++ src/test/java/ru/netology/geo/GeoServiceImplTest.java
@@ -1,12 +1,10 @@
-package ru.netology;
+package ru.netology.geo;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.netology.entity.Country;
 import ru.netology.entity.Location;
-import ru.netology.geo.GeoService;
-import ru.netology.geo.GeoServiceImpl;
 
 public class GeoServiceImplTest {
     private  Location location;
--- src/test/java/ru/netology/i18n/LocalizationServiceImplTest.java
+++ src/test/java/ru/netology/i18n/LocalizationServiceImplTest.java
@@ -1,11 +1,9 @@
-package ru.netology;
+package ru.netology.i18n;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.netology.entity.Country;
-import ru.netology.i18n.LocalizationService;
-import ru.netology.i18n.LocalizationServiceImpl;
 
 public class LocalizationServiceImplTest {
     private LocalizationService localizationService;
